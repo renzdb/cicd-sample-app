02c3e43323ba0ca6847fab0ecfaf447dbe5af474
Renz De Baets <renz.debaets@student.hogent.be>
2025-10-07 17:26:27 +0200

    Add Jenkinsfile for automated pipeline

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,23 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Build Sample App') {
+            steps {
+                echo 'Building the sample app...'
+                sh 'bash ./sample-app.sh'
+            }
+        }
+
+        stage('Test Sample App') {
+            steps {
+                echo 'Testing the sample app...'
+                sh '''
+                    APP_IP=172.17.0.3
+                    JENKINS_IP=172.17.0.2
+                    curl http://$APP_IP:5050/ | grep "You are calling me from $JENKINS_IP"
+                '''
+            }
+        }
+    }
+}
